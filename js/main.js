b5758697bc9d9233cf8a85cd112e6169380b7163
Will W <wjwat@onionslice.org>
2022-01-27 06:26:51 -0600

    Remove unnecessary comments

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-// UTILITY LOGIC
-
 // This works for most of what we care about.
 // An edge case though is if the val has a trailing .
 // ex: isNotNumber('123.') -> true
@@ -11,16 +9,7 @@ function listNumber(value, index) {
   return `<li title="${index}">${value}</li>`;
 }
 
-// BUSINESS LOGIC
-
-// Return an array constructed using the following constraints that our
-// UI logic can iterate over to display messages to our user.
-//
-// Numbers that contain a 1: all digits are replaced (all digits) with "Beep!"
-// Numbers that contain a 2: all digits are replaced (all digits) with "Boop!"
-// Numbers that contain a 3: all digits are replaced (all digits) with "Won't you be my neighbor?"
 function beepBoop(userInput) {
-  // XXX '2.5' -> 2, Is this the desired behavior when given a float?
   let num = parseInt(userInput);
   let outputArray = [];
 
@@ -28,7 +17,6 @@ function beepBoop(userInput) {
     return ["Please input a valid number."];
   }
 
-  // Always loop num+1 to correctly size our output
   for (let i = 0; i < num+1; i++) {
     if (i.toString().indexOf('3') > -1) {
       outputArray.push("Won't you be my neighbor?");
@@ -46,9 +34,6 @@ function beepBoop(userInput) {
 
 // UI LOGIC
 $(document).ready(function() {
-  // Assign display selector to a variable because we're going to use
-  // it in multiple places and what we're selecting may change
-  // as we refactor.
   let display = $('#output');
   let input = $('#input');
   let ordering = '';
